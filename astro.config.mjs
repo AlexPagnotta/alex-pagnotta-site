bbf0317f6fe8f34e7065e13709971c54d84ba193
Alex Pagnotta <ap.alex.pagnotta@gmail.com>
2022-07-20 23:32:42 +0200

    feat: add preact integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
-import tailwind from '@astrojs/tailwind'
-import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config';
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({ config: { applyBaseStyles: false } })],
-})
+  integrations: [tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), preact()]
+});
